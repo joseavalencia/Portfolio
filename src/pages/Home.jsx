17f1758c7eccd0bbbe54018fe0d7997eab2d28a8
joanvale <joanvale@ucsc.edu>
2025-07-04 20:36:03 -0700

    add web app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { ThemeToggle } from "../componenets/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
